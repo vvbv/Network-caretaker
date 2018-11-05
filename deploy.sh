f00067036ba207567d3fdae93bd1c8fc1e7328ae
vvbv <jeison.cardona@correounivalle.edu.co>
2018-11-04 23:21:41 -0500

    mejoras en el deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
-cd src
-rm main
+#!/bin/bash
 clear
-rustc main.rs
-./main
+echo "Clearing old binaries"
+rm build/network_observant > /dev/null
+echo "Clearing old binaries [Completed]"
+echo "Compiling"
+rustc src/main.rs -o build/network_observant
+echo "Compiling [Completed]"
+echo "Running\n"
+build/network_observant
+echo "\nDone, return code $?"
