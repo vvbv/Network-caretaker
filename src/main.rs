d6e9c883a5b072b0884ed503131e1cd0781b2a23
vvbv <jeison.cardona@correounivalle.edu.co>
2018-11-05 01:46:58 -0500

    pruebas

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,17 @@ fn main() {
     //let host_list_ip_mac = nmap_manager::execute_nmap( "192.168.1.1".to_string() );
     //let host_list_ip_mac_manofacturer = macvendor_manager::get_vendors( host_list_ip_mac );
     //println!("{:?}",host_list_ip_mac_manofacturer)
-    database_manager::write_database( 
-        "postgres".to_string(), 
-        "postgres".to_string(), 
-        "rust_test".to_string(),
-        "localhost".to_string(),
-        5432 );
+
+    let db_params: database_manager::Connection_params = database_manager::Connection_params{
+        user: "postgres".to_string(),
+        password: "postgres".to_string(),
+        db_name: "rust_test".to_string(),
+        host: "localhost".to_string(),
+        port: 5432
+    };
+    
+    let conn = database_manager::get_connection( &db_params );
+
+    database_manager::write_database( &conn );
+    database_manager::record_macvendors_log( &conn );
 }
